756ec34c31f59e00c215a7a5d6dcffa8908bb946
ojedapedro <ojedapedro2302741@gmail.com>
2025-10-18 16:13:52 -0400

    Update API_BASE URL in config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // URL del Apps Script web app (reemplaza por tu URL desplegada si diferente)
-const API_BASE = "https://script.google.com/macros/s/AKfycby5P_X-2JjonYi3VifmtZ9YRZoA0VJaXt2XKmiY9RGkT6E0SQ7wqXAT-Odi0QYTXnU0/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbwfZXoFtvDHiK3OxibrFABA3Ul_012LVcnadBwn3Q5sHJcPd5QJnQ83DJu71OZNiGsp/exec";
+
 
